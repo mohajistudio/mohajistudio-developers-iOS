e281c50bcb29b0d6b1c4c75346fccacdeb7da0ce
SongKyuSeob <sks5075@naver.com>
2025-03-08 01:17:23 +0900

    Feat :: 프로필 페이지 수정 기능 추가 관련 UI 구현
    # 🤔 배경
    
    탈퇴 버튼을 구현할 겸 에딧 모드에 따라 UI의 변하는 부분까지 이번에 어떤식으로 구현할지 배워볼겸 시도해보았습니다.
    
    # 📃 작업 내역
    
    - 전반적 UI 수정사항 반영
    - 네트워킹 메서드에 관련된 부분을 제외하곤 UI적으로 전체적으로 구현 완료
    
    # ✅ 리뷰 노트
    
    뷰를 조금 더 유연하게 쓸 수 있는 방법론 하나를 알게되었습니다. 기존 height를 가변적으로 처리함에 있어 많이 버벅였는데 서브뷰와 상위뷰와의 관계를 조금 더 깊게 이해할 수 있는 기회를 통해 가변적인 높이를 가지는 컴포넌트를 좀 더 잘 다룰 수 있게 된 것 같습니다. 하지만 여전히 상태에 기반한 전체적으로 실시간 UI 수정을 함에 있어 떠오르지않아 AI의 힘을 빌리고 이를 분석하는 데에 시간을 썼습니다. 조금 더 이 관계와 아이디어를 도출해내는 연습이 필요한 것 같습니다.

--- mohajistudio-developers-iOS/mohajistudio-developers-iOS/Presentation/Scenes/ProfileDetail/ViewControllers/ProfileViewController.swift
+++ mohajistudio-developers-iOS/mohajistudio-developers-iOS/Presentation/Scenes/ProfileDetail/ViewControllers/ProfileViewController.swift
@@ -17,6 +17,7 @@ class ProfileViewController: UIViewController {
         
         let testUserDetail = UserDetailResponse(id: "1", nickname: "Kyu_Veloper", email: "sks565075@gmail.com", profileImageUrl: "", bio: "음악을 사랑하는 개발 지망생입니다!\n iOS 플랫폼 위주로 하고 있습니다.", role: "iOS Native Developer", contacts: nil)
         
+        // 유저 데이터 불러오는 로직 필요
         profileView.configure(userDetail: testUserDetail, contacts: viewModel.contacts)
     }
     
--- mohajistudio-developers-iOS/mohajistudio-developers-iOS/Presentation/Scenes/ProfileDetail/Views/ProfileView.swift
+++ mohajistudio-developers-iOS/mohajistudio-developers-iOS/Presentation/Scenes/ProfileDetail/Views/ProfileView.swift
@@ -6,9 +6,16 @@
 //
 
 import UIKit
+import SnapKit
 
 class ProfileView: UIView {
 
+    private var isEditMode: Bool = false
+    
+    private var dynamicButtonHeightConstraint: Constraint?
+    
+    private let profileCardView = ProfileCardView()
+    
     private let scrollView = UIScrollView()
     
     private let contentView = UIView().then {
@@ -23,37 +30,14 @@ class ProfileView: UIView {
         $0.configure(iconName: "User", headerTitle: "Profile")
     }
     
+    private let editModeButton = UIButton().then {
+        $0.setImage(UIImage(named: "Post_edit"), for: .normal)
+        $0.tintColor = UIColor(named: "Primary")
+    }
+    
     private let profileBackgroundView = UIView().then {
         $0.backgroundColor = UIColor(named: "Surface 1")
         $0.layer.cornerRadius = 16
-    }
-    
-    private let nameLabel = UILabel().then {
-        $0.font = UIFont(name: "Pretendard-Bold", size: 20)
-        $0.text = "Name"
-        $0.textColor = UIColor(named: "Black")
-        $0.textAlignment = .left
-    }
-    
-    private let roleLabel = UILabel().then {
-        $0.font = UIFont(name: "Pretendard-Medium", size: 14)
-        $0.text = "Developer"
-        $0.textColor = UIColor(named: "Gray 3")
-        $0.textAlignment = .left
-    }
-    
-    private let introduceLabel = UILabel().then {
-        $0.numberOfLines = 2
-        $0.font = UIFont(name: "Pretendard-Medium", size: 14)
-        $0.textColor = UIColor(named: "Gray 1")
-        $0.text = "Introduce"
-        $0.textAlignment = .left
-    }
-    
-    private let profileImageView = UIImageView().then {
-        $0.image = UIImage(named: "TempProfileImage")
-        $0.layer.cornerRadius = 8
-        $0.clipsToBounds = true
     }
     
     private let contactHeaderView = HeaderWithIconView().then {
@@ -62,11 +46,54 @@ class ProfileView: UIView {
     
     private let contactsView = ContactsView()
     
+    private let dynamicButtonView = UIView() // 취소 저장 버튼을 담을 뷰
+    
+    private let cancelButton = UIButton().then {
+        $0.setTitle("취소", for: .normal)
+        $0.titleLabel?.font = UIFont(name: "Pretendard-Medium", size: 14)
+        $0.backgroundColor = UIColor(named: "Surface 1")
+        $0.setTitleColor(UIColor(named: "Gray 1"), for: .normal)
+        $0.layer.cornerRadius = 8
+    }
+    
+    private let saveButton = UIButton().then {
+        $0.setTitle("저장", for: .normal)
+        $0.titleLabel?.font = UIFont(name: "Pretendard-Medium", size: 14)
+        $0.backgroundColor = UIColor(named: "Success")
+        $0.setTitleColor(UIColor(named: "white"), for: .normal)
+        $0.layer.cornerRadius = 8
+    }
+    
+    private let deleteAccountBlock = UIView().then {
+        $0.backgroundColor = .clear
+    }
+    
+    private let deleteAccountHeaderView = HeaderWithIconView().then {
+        $0.configure(iconName: "Logout", headerTitle: "Delete Account")
+    }
+    
+    private let deleteAccountButton = UIButton().then {
+        $0.setTitle("회원탈퇴", for: .normal)
+        $0.titleLabel?.font = UIFont(name: "Pretendard-Medium", size: 14)
+        $0.setTitleColor(UIColor(named: "Error"), for: .normal)
+        $0.backgroundColor = UIColor(named: "Surface 1")
+    }
+    
+    private let bottomStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 40
+        $0.distribution = .fill
+    }
+    
+    private var originalUserInfo: (name: String, career: String, bio: String)?
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         setupUI()
+        setupAction()
         
+        profileCardView.setViewMode()
     }
     
     required init?(coder: NSCoder) {
@@ -76,6 +103,7 @@ class ProfileView: UIView {
     private func setupUI() {
         setupHierarchy()
         setupConstraints()
+        setupBottomStackView()
     }
     
     private func setupHierarchy() {
@@ -84,15 +112,15 @@ class ProfileView: UIView {
         
         contentView.addSubview(topDivider)
         contentView.addSubview(profileHeaderView)
+        contentView.addSubview(editModeButton)
         
         contentView.addSubview(profileBackgroundView)
-        profileBackgroundView.addSubview(profileImageView)
-        profileBackgroundView.addSubview(nameLabel)
-        profileBackgroundView.addSubview(roleLabel)
-        profileBackgroundView.addSubview(introduceLabel)
+        profileBackgroundView.addSubview(profileCardView)
         
         contentView.addSubview(contactHeaderView)
         contentView.addSubview(contactsView)
+        
+        contentView.addSubview(bottomStackView)
     }
 
     private func setupConstraints() {
@@ -118,31 +146,21 @@ class ProfileView: UIView {
             $0.height.equalTo(24)
         }
         
+        editModeButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.centerY.equalTo(profileHeaderView)
+            $0.size.equalTo(24)
+        }
+        
         profileBackgroundView.snp.makeConstraints {
             $0.horizontalEdges.equalToSuperview().inset(20)
             $0.top.equalTo(profileHeaderView.snp.bottom).offset(12)
-            $0.bottom.equalTo(introduceLabel.snp.bottom).offset(32)
+            $0.bottom.equalTo(profileCardView.snp.bottom)
         }
         
-        profileImageView.snp.makeConstraints {
-            $0.horizontalEdges.equalToSuperview().inset(20)
-            $0.top.equalToSuperview().offset(20)
-            $0.height.equalTo(profileImageView.snp.width)
-        }
-        
-        nameLabel.snp.makeConstraints {
-            $0.top.equalTo(profileImageView.snp.bottom).offset(16)
-            $0.horizontalEdges.equalTo(profileImageView)
-        }
-        
-        roleLabel.snp.makeConstraints {
-            $0.top.equalTo(nameLabel.snp.bottom).offset(4)
-            $0.horizontalEdges.equalTo(nameLabel)
-        }
-        
-        introduceLabel.snp.makeConstraints {
-            $0.top.equalTo(roleLabel.snp.bottom).offset(16)
-            $0.horizontalEdges.equalTo(roleLabel)
+        profileCardView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.top.equalToSuperview()
         }
         
         contactHeaderView.snp.makeConstraints {
@@ -154,27 +172,137 @@ class ProfileView: UIView {
         contactsView.snp.makeConstraints {
             $0.horizontalEdges.equalTo(contactHeaderView)
             $0.top.equalTo(contactHeaderView.snp.bottom).offset(12)
-            $0.bottom.equalTo(contactsView.getContactStackView().snp.bottom).offset(20)
+        }
+        
+        bottomStackView.snp.makeConstraints {
+            $0.top.equalTo(contactsView.snp.bottom).offset(40)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.centerX.equalToSuperview()
         }
         
         contentView.snp.makeConstraints {
-            $0.bottom.equalTo(contactsView.snp.bottom).offset(20)
+            $0.bottom.equalTo(bottomStackView.snp.bottom).offset(20)
+        }
+    }
+    
+    func setupBottomStackView() {
+        
+        dynamicButtonView.addSubview(saveButton)
+        dynamicButtonView.addSubview(cancelButton)
+        
+        bottomStackView.addArrangedSubview(dynamicButtonView)
+        bottomStackView.addArrangedSubview(deleteAccountBlock)
+        
+        dynamicButtonView.snp.makeConstraints {
+            dynamicButtonHeightConstraint = $0.height.equalTo(0).constraint
+            $0.horizontalEdges.equalToSuperview()
+        }
+        
+        cancelButton.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.verticalEdges.equalToSuperview()
+            $0.trailing.equalTo(dynamicButtonView.snp.centerX).offset(-4)
+        }
+        
+        saveButton.snp.makeConstraints {
+            $0.leading.equalTo(dynamicButtonView.snp.centerX).offset(4)
+            $0.trailing.equalToSuperview()
+            $0.verticalEdges.equalToSuperview()
+        }
+        
+        saveButton.isHidden = true
+        cancelButton.isHidden = true
+        
+        deleteAccountBlock.addSubview(deleteAccountHeaderView)
+        deleteAccountBlock.addSubview(deleteAccountButton)
+        
+        deleteAccountHeaderView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(24)
+        }
+        
+        deleteAccountButton.snp.makeConstraints {
+            $0.top.equalTo(deleteAccountHeaderView.snp.bottom).offset(12)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(40)
+            $0.bottom.equalToSuperview()
+        }
+        
+    }
+    
+    private func setupAction() {
+        editModeButton.addTarget(self, action: #selector(didTapEditModeButton), for: .touchUpInside)
+        cancelButton.addTarget(self, action: #selector(didTapCancelButton), for: .touchUpInside)
+        saveButton.addTarget(self, action: #selector(didTapSaveButton), for: .touchUpInside)
+    }
+    
+    @objc private func didTapEditModeButton() {
+        profileCardView.toggleCurrentMode()
+        editModeButton.isHidden = true
+        
+        saveButton.isHidden = false
+        cancelButton.isHidden = false
+        
+        dynamicButtonHeightConstraint?.update(offset: 32)
+        
+        let userInfo = profileCardView.getEditedUserInfo()
+        originalUserInfo = userInfo
+        
+        UIView.animate(withDuration: 0.3) {
+            self.layoutIfNeeded()
+        }
+    }
+    
+    @objc private func didTapSaveButton() {
+        self.profileCardView.toggleCurrentMode()
+        self.editModeButton.isHidden = false
+        
+        saveButton.isHidden = true
+        cancelButton.isHidden = true
+        dynamicButtonHeightConstraint?.update(offset: 0)
+        
+        UIView.animate(withDuration: 0.3) {
+            self.layoutIfNeeded()
+        }
+        
+        // 수정된 사용자 정보 가져와 UI 반영
+        let editedInfo = profileCardView.getEditedUserInfo()
+        // 이후 업데이트 로직 수행
+    }
+    
+    @objc private func didTapCancelButton() {
+        self.profileCardView.toggleCurrentMode()
+        self.editModeButton.isHidden = false
+        
+        saveButton.isHidden = true
+        cancelButton.isHidden = true
+        
+        dynamicButtonHeightConstraint?.update(offset: 0)
+        
+        UIView.animate(withDuration: 0.3) {
+            self.layoutIfNeeded()
+        }
+        // 수정 취소니 이전 값 다시 돌려주는 로직 필요. 에딧 모드로 전환 시 기존 값 따로 저장해둬야함.
+        if let originalUserInfo = originalUserInfo {
+            profileCardView.configureUserInfo(name: originalUserInfo.name,
+                                              career: originalUserInfo.career,
+                                              bio: originalUserInfo.bio,
+                                              profileImageUrl: "")
         }
     }
     
     func configure(userDetail: UserDetailResponse, contacts: [Contact]) {
         // profileImageView에 프로필 이미지 링크 메서드
-        nameLabel.text = userDetail.nickname
-        roleLabel.text = userDetail.role
-        introduceLabel.text = userDetail.bio
+        if let profileImageUrl = userDetail.profileImageUrl {
+            profileCardView.configureUserInfo(name: userDetail.nickname, career: userDetail.role, bio: userDetail.bio, profileImageUrl: profileImageUrl)
+        } else {
+            profileCardView.configureUserInfo(name: userDetail.nickname, career: userDetail.role, bio: userDetail.bio, profileImageUrl: "")
+        }
         
         contactsView.configure(contacts: contacts)
         
-        guard let profileImageUrl = userDetail.profileImageUrl, profileImageUrl != "" else {
-            profileImageView.image = UIImage(named: "Default_profile")
-            return
-        }
-        
     }
+    
     
 }
